c0de4c8777e033686b57a727c5917a62b8eea83a
Jay R Bolton <jayrbolton@gmail.com>
2020-07-07 12:03:35 -0700

    Add comment

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ def a_mocked():
     print('hello from a_mocked')
 
 
+# Note that to mock a_fn inside module `b`, you must mock the path `src.b.a_fn`
 with patch('src.b.a_fn', return_value='mocked!', side_effect=a_mocked):
     b_fn()
